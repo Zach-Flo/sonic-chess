7e9479107aa234e971e463d6ca3cbe3f1cf72d62
Zach Florian <zachary.florian@uconn.edu>
2023-11-12 22:31:14 -0500

    random moves until game over

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,51 @@
 'use client'
 import { Chessboard } from "react-chessboard";
-export default function App() {
+import { useState } from "react";
+import { Chess } from "Chess.js";
+
+interface GameProps {
+  chess: Chess;
+  board: any;
+}
+
+function MakeAMove({ chess, board }: GameProps){
+  if (!chess.isGameOver()){
+    const moves = chess.moves();
+    const move = moves[Math.floor(Math.random() * moves.length)];
+    chess.move(move);
+    setTimeout(() => { console.log("move made");}, 500);
+  }
+  return chess.fen()
+
+}
+
+function Game({ chess, board }: GameProps){
+  
+  console.log(chess.pgn());
   return(
     <>
-      <Chessboard position={"start"} />
+      <Chessboard position={board}></Chessboard>
+    </>
+  );
+}
+
+
+export default function App() {
+  const [chess, setChess] = useState(new Chess());
+  const [board, setBoard] = useState(chess.fen());
+
+  function handleClick(){
+    setBoard(MakeAMove({chess, board}))
+  }
+  return(
+    <>
+      <div className=" max-w-xl">
+        <Game chess={chess} board={board}></Game>
+        <button onClick={handleClick}>Move</button>
+      </div>
     </>
   ) 
 
 }
+
+
